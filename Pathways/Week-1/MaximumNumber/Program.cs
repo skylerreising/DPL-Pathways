3d8c7a6d6538c54b337473e13121ffae384eec56
Skyler Reising <skyler.reising@gmail.com>
2024-01-03 12:22:37 -0600

    Small improvements in comments MaximumNumber

--- Pathways/Week-1/MaximumNumber/Program.cs
+++ Pathways/Week-1/MaximumNumber/Program.cs
@@ -7,7 +7,7 @@ Last update: January 2nd, 2024
 
 Requirements:
 (1) All numbers in the problem should be integers.
-(2) The number of numbers should be between and include 1-5.
+(2) The amount of numbers should be between and include 1-5.
 (3) Each number given should be between and include -100 and 100.
 (4) Use variables instead of arrays or other objects.
 
@@ -15,8 +15,8 @@ Algorithm:
 do
 (1) Prompt the user for the number of numbers between and including 1-5.
     (1a) While number isn't between or including 1-5, error message and prompt for number again.
-(2) Store the number of numbers in a variable i because this will determine how many times we ask for a number.
-(3) Declare a variable maxNum and assign it the lowest possible value.
+(2) Store the number of numbers in a variable amount because this will determine how many times we ask for a number.
+(3) Declare a variable maxNum and assign it the lowest possible value. (-100)
 (4) Ask the user for each number and store that number in maxNum if that number is greater than maxNum.
 (5) Write maxNum to the console.
 
@@ -61,7 +61,7 @@ namespace NumberComparisons
                         //Prompt the user the number they want compared
                         Console.WriteLine("Please enter a number you want compared between and including -100 and 100.");
 
-                        //declare a variable to hold each number the user wants compared
+                        //assign the variable to hold each number the user wants compared
                         userNum = Convert.ToInt32(Console.ReadLine());
 
                         //if number isn'g between -100 and 100, error message and prompt for number again
