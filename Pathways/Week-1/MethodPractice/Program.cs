44a745f024dad6acc628f2722b4423f2da4c472b
Skyler Reising <skyler.reising@gmail.com>
2024-01-03 12:53:18 -0600

    Mid day Method Practice

---
+++ Pathways/Week-1/MethodPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+  class Program  
+  {
+    static int InputAndValidateInteger (int minValue, int maxValue, string prompt)
+    {
+    int value;
+
+    do
+    {
+        Console.WriteLine(prompt + " between " + minValue + " and " + maxValue);
+        value = Convert.ToInt32(Console.ReadLine());
+        if ((value < minValue) || (value > maxValue))
+            Console.WriteLine("Oops! the value needs to be between " + minValue + " and " + maxValue);
+    } while ((value < minValue) || (value > maxValue));
+    
+    return value;
+    }
+
+    public static void Main(string[] args)
+    {
+      int numberOfNumbers = 0, numberValue = 0, sum = 0; 
+      
+      numberOfNumbers = InputAndValidateInteger(1,5,"Please enter a number 1 - 5");
+
+        //Add the number values entered together and display the outcome.
+      for (int i = 1; i <= numberOfNumbers; i++)
+       {
+         numberValue = InputAndValidateInteger(-100,100, "Please enter a number -100 - 100");
+
+        sum += numberValue;
+
+       } //end for loop
+      
+      Console.WriteLine("The total = " + sum);
+    }
+}
