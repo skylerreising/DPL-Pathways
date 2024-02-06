8c6e7fcfc1f2b3e9aedbc7bf404920efbf009482
Skyler Reising <skyler.reising@gmail.com>
2024-02-06 16:54:08 -0600

    Update to exercise

--- Pathways/Stage 2/Week-1/Palindrome/main.js
+++ Pathways/Stage 2/Week-1/Palindrome/main.js
@@ -13,10 +13,25 @@ function getNewNumber(theNewWord){
 
 function addWordToList(word,num){
     if(num === "1"){
-        document.querySelector("#myList1").appendChild(document.createElement("tr")).innerHTML = word;
+        document.querySelector("#myList1").appendChild(document.createElement("tr")).innerHTML = `${word}: ${palindrome1(word)}`;
     }else{
-        document.querySelector("#myList2").appendChild(document.createElement("tr")).innerHTML = word;
+        document.querySelector("#myList2").appendChild(document.createElement("tr")).innerHTML = `${word}: ${palindrome2(word)}`;
     }
+}
+
+function palindrome1(word){
+    return word.split("").reverse().join("") === word ? "true":"false";
+}
+
+function palindrome2(word){
+    for(let i=0; i<Math.ceil(word.length/2); i++){
+        if(word[i] === word[word.length - 1 - i]){
+            continue;
+        }else{
+            return "false";
+        }
+    }
+    return "true";
 }
 
 function clearList1(){
