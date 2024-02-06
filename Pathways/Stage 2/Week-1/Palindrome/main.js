9bf0a548ddd3873409d8f42c2a63412b95761b37
Skyler Reising <skyler.reising@gmail.com>
2024-02-06 16:29:19 -0600

    Today's work.

---
+++ Pathways/Stage 2/Week-1/Palindrome/main.js
@@ -0,0 +1,32 @@
+function validateANDadd(){
+    //find new word and if its empty, alert
+    let theNewWord = document.forms["myForm"]["newWord"].value;
+
+    theNewWord === "" ? alert("You must enter a word!") : getNewNumber(theNewWord);
+}
+
+function getNewNumber(theNewWord){
+    let theNewNum = document.forms["myForm"]["newNumber"].value;
+
+    (theNewNum === "1" || theNewNum === "2") ? addWordToList(theNewWord, theNewNum) : alert("You must enter a 1 or 2!");
+}
+
+function addWordToList(word,num){
+    if(num === "1"){
+        document.querySelector("#myList1").appendChild(document.createElement("tr")).innerHTML = word;
+    }else{
+        document.querySelector("#myList2").appendChild(document.createElement("tr")).innerHTML = word;
+    }
+}
+
+function clearList1(){
+    //get the list
+    //set the inner html to nothing
+    document.querySelector("#myList1").innerHTML = "";
+}
+
+function clearList2(){
+    //get the list
+    //set the inner html to nothing
+    document.querySelector("#myList2").innerHTML = "";
+}
