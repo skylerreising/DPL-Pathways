55b66958576d4b2e313f7a939a48a61e16f9c71c
Skyler Reising <skyler.reising@gmail.com>
2024-02-07 09:21:51 -0600

    Added 3rd palindrome check to the app

--- Pathways/Stage 2/Week-1/Palindrome/main.js
+++ Pathways/Stage 2/Week-1/Palindrome/main.js
@@ -1,6 +1,8 @@
 document.querySelector("#addToList").addEventListener("click", validateANDadd)
 document.querySelector("#clearList1").addEventListener("click", clearList1)
 document.querySelector("#clearList2").addEventListener("click", clearList2)
+document.querySelector("#clearList3").addEventListener("click", clearList3)
+
 
 function validateANDadd(){
     let theNewWord = document.forms["myForm"]["newWord"].value;
@@ -9,14 +11,18 @@ function validateANDadd(){
 }
 
 function checkNumber(theNewWord, theNewNum){
-    (theNewNum === "1" || theNewNum === "2") ? addWordToList(theNewWord, theNewNum) : alert("You must enter a 1 or 2!");
+    (theNewNum === "1" || theNewNum === "2" || theNewNum === "3") ? addWordToList(theNewWord, theNewNum) : alert("You must enter a 1, 2, or 3!");
 }
 
+//can I add the word and palindrome bool as different columns in a table?
 function addWordToList(word,num){
     if(num === "1"){
         document.querySelector("#myList1").appendChild(document.createElement("tr")).innerHTML = `${word}: ${palindrome1(word)}`;
-    }else{
+    }else if(num === "2"){
         document.querySelector("#myList2").appendChild(document.createElement("tr")).innerHTML = `${word}: ${palindrome2(word)}`;
+    }else{
+        document.querySelector("#myList3").appendChild(document.createElement("tr")).innerHTML = `${word}: ${palindrome3(word)}`;
+
     }
 }
 
@@ -35,10 +41,18 @@ function palindrome2(word){
     return "true";
 }
 
+function palindrome3(word){
+    return word.split("").reverse().join("").toLowerCase() === word.toLowerCase() ? "true":"false";
+}
+
 function clearList1(){
     document.querySelector("#myList1").innerHTML = "";
 }
 
 function clearList2(){
     document.querySelector("#myList2").innerHTML = "";
+}
+
+function clearList3(){
+    document.querySelector("#myList3").innerHTML = "";
 }
