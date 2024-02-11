90b2e082ecd75c58e30e7cb418f087ff1e51ce10
Skyler Reising <skyler.reising@gmail.com>
2024-02-11 10:18:28 -0600

    Separated delete and complete and both are working.

--- Pathways/Stage 2/Week-1/W1CompChalProb/main.js
+++ Pathways/Stage 2/Week-1/W1CompChalProb/main.js
@@ -1,23 +1,27 @@
 document.querySelector("#addToList").addEventListener("click", postTodo);
 document.querySelector("#clearList").addEventListener("click", clearList);
-document.querySelector("#myList").addEventListener("click", deleteOrComplete)
+// document.querySelector("#myList").addEventListener("click", deleteOrComplete)
 
 //variable to hold my list
 const myList = document.querySelector("#myList");
 
 function postTodo(){
     //checkmark
-    const checkmark = "<i class='fa-regular fa-square-check'></i>"
+    const checkmark = "<i class='fa-regular fa-square-check checkMark'></i>"
 
     //todo item
     let todo = document.querySelector("#todoEntry").value;
 
     //trashcan
-    const trashcan = '<i class="fa-solid fa-trash-can"></i>'
+    const trashcan = '<i class="fa-solid fa-trash-can trashCan"></i>'
     
     //Add all 3 to the list
-    //trashcan
-    myList.appendChild(document.createElement("tr")).appendChild(document.createElement("td")).innerHTML = checkmark;
+    //checkmark
+    let checkMarkCell = myList.appendChild(document.createElement("tr")).appendChild(document.createElement("td"));
+    checkMarkCell.innerHTML = checkmark;
+
+    //add event listener to checkmark
+    checkMarkCell.querySelector(".checkMark").addEventListener("click", completeOrUncomplete);
 
     //todo item
     //find the last tr and add another td with the todo item
@@ -30,21 +34,37 @@ function postTodo(){
     lastRow.appendChild(document.createElement("td")).innerHTML = todoText;
 
     //trashcan
-    lastRow.appendChild(document.createElement("td")).innerHTML = trashcan;
+    let trashCanCell = lastRow.appendChild(document.createElement("td"));
+    trashCanCell.innerHTML = trashcan;
+
+    //add event listener to trashcan
+    trashCanCell.querySelector(".trashCan").addEventListener("click", deleteTodo);
 }
 
 function clearList(){
     document.querySelector("#myList").innerHTML = "";
 }
 
-function deleteOrComplete(){
-    myList.addEventListener("click", function (e){
-        if(e.target.classList.contains("fa-trash-can")){
-            let todoRow = e.target.closest('tr')
-            todoRow.remove();
-        }else if(e.target.classList.contains("fa-square-check")){
-            let todoData = e.target.closest('tr').querySelector(".todo-text");
-            todoData.classList.toggle("completed");
-        }
-    })
+//delete by itself
+function deleteTodo(e){
+    e.target.closest('tr').remove();
 }
+
+//fix complete and uncomplete
+function completeOrUncomplete(e){
+    let todoData = e.target.closest('tr').querySelector(".todo-text");
+    todoData.classList.toggle("completed");
+}
+
+//old code that only marked complete/uncomplete every other click. Delete however worked.
+// function deleteOrComplete(){
+//     myList.addEventListener("click", function (e){
+//         if(e.target.classList.contains("fa-trash-can")){
+//             let todoRow = e.target.closest('tr')
+//             todoRow.remove();
+//         }else if(e.target.classList.contains("fa-square-check")){
+//             let todoData = e.target.closest('tr').querySelector(".todo-text");
+//             todoData.classList.toggle("completed");
+//         }
+//     })
+// }
