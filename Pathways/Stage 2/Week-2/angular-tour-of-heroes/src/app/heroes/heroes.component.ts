f9bfb763205048b03a49251e28377a1c957c9ad3
Skyler Reising <skyler.reising@gmail.com>
2024-02-19 10:02:13 -0600

    Updated heroes.component

--- Pathways/Stage 2/Week-2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Pathways/Stage 2/Week-2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
