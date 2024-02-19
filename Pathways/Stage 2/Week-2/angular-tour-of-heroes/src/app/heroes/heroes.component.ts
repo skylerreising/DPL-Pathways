c76a0e4a78d7fcee5eec5aa955685dfcb37df34d
Skyler Reising <skyler.reising@gmail.com>
2024-02-19 09:34:15 -0600

    Tour of Heroes first commit

---
+++ Pathways/Stage 2/Week-2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class  HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
