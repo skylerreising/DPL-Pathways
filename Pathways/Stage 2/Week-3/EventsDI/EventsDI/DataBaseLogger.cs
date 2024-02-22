60df2e394055d3ffb7c9b928b87e8d77cd056030
Skyler Reising <skyler.reising@gmail.com>
2024-02-22 15:25:47 +0000

    Dependency Injection Exercise

---
+++ Pathways/Stage 2/Week-3/EventsDI/EventsDI/DataBaseLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDI
+{
+    public class DataBaseLogger: ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Using the Log method of the DataBaseLogger");
+            LogToDatabase(message);
+        }
+
+        public void LogToDatabase(string message)
+        {
+            Console.WriteLine($"Method: LogToDatabase, Test: {message}\n");
+        }
+    }
+}
---
+++ Pathways/Stage 2/Week-3/EventsDI/EventsDI/PapyrusLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDI
+{
+    public class PapyrusLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Using the Log method of the PapyrusLogger");
+            LogToPapyrus(message);
+        }
+
+        public void LogToPapyrus(string message)
+        {
+            Console.WriteLine($"Method: LogToPapyrus, Test: {message}\n");
+        }
+    }
+}
